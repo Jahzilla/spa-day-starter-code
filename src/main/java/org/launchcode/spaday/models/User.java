c8a8e6e2191dedbfbe0e2008989220fa62ce7360
Jarred <jarred.spencer88@gmail.com>
2021-11-04 20:59:42 -0400

    added server side validation for add.html

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,38 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Cannot be blank.")
+    @NotNull(message = "Cannot be null.")
+    @Size(min = 5, max = 15, message = "Minimum characters must be 5. Max characters must be 15.")
     private String username;
+
+    @Email
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters.")
+    @NotBlank(message = "Cannot be blank.")
+    @NotNull(message = "Cannot be null.")
     private String password;
 
+    @Size(min = 6, message = "Password must be at least 6 characters.")
+    @NotBlank(message = "Cannot be blank.")
+    @NotNull(message = "Cannot be null.")
+    private String verify;
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +57,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
